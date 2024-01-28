9e95d9ee53da39a51ab83cb1ed64c69ad5ccb3cb
MReza <118252364+rezamontazeri76@users.noreply.github.com>
2024-01-28 15:27:55 +0330

    Add files via upload

---
+++ syntax examples/for.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..128000}   #frome 1 to 128000 do smthing...
+
+do                     #do...
+
+   touch test$i        #creat the file 
+
+   echo "test$i has been created"     #write the name of every file have been created
+
+done                    #end
